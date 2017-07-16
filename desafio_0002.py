ce4f0133d9c4c38b0fb83121206ec6acd5da27da
Luciano Silva <luciano.tj2011@gmail.com>
2017-07-15 22:46:40 -0300

    Dois primeiros desafios concluidos

---
+++ desafio_0002.py
@@ -0,0 +1,10 @@
+# 2: No modo interativo armazene uma string 'exemplo  ' em uma variável
+# com espaço em branco, depois exiba o resultado na tela eliminando o espaço em branco.
+
+s = 'exemplo   '
+s1 = s.strip()
+
+print("Antes: " + s + "_algo depois")
+
+print("Antes: " + s1 + "_algo depois")
+
